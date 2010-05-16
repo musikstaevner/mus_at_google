72149e21cd15c3fac0df8de62620a7f1ad1c33df
Martin Hansen <martin@homer3>
2010-05-16 04:35:05 +0200

    working tilmelding parser and response

--- muss_auto_reply_parser.py
+++ muss_auto_reply_parser.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
 
 
 import logging, email
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import mail
+
 from datamodel import ElevInfo
 
 # equals an enum to govern the path in my statemachine
@@ -14,7 +18,7 @@ data = {}
 class FormHandler(InboundMailHandler):
     
     
-    def store(self):
+    def store_tilmelding(self):
         self.request.charset='utf-8'
         elev = ElevInfo()
 
@@ -34,9 +38,122 @@ class FormHandler(InboundMailHandler):
         elev.ForaeldreEpost = data[Fepost]
         elev.ForaeldreFastnet = data[Ffnum]
         elev.ForaeldrMobil = data[Fmnum]
-        elev.Besked = data[Besked]
+        elev.Besked = data[Besked][:500]
 
         elev.put()
+
+    def respond_tilmelding(self):
+        elev_txt = u"""
+Elev:
+
+
+%s
+%s
+%s
+
+E-post: %s
+Mobil: %s
+Cpr: %s
+Spiller %s på %s niveau
+ønsker enkeltværelse: %s
+Søskenderabat: %s
+        """ % (data[Enavn], data[Eadr], data[Epostby], data[Eepost], data[Emobil], data[Ecpr],
+               data[Einstrument], data[Eniveau], data[Eene], data[Erabat] )
+
+        far_txt = u"""
+Forældre/Værge
+
+
+%s
+%s
+%s
+
+E-post: %s
+Fastnet: %s
+Mobil: %s
+
+        """ % (data[Fnavn], data[Fadr], data[Fpostby], data[Fepost], data[Ffnum], data[Fmnum])
+
+        if data.has_key(Besked):
+            besked_txt =u"Besked:\n%s" % data[Besked]
+        else:
+            besked_txt =u"Ingen besked"
+        
+        oplysninger = u"""
+Herunder er de oplysninger vi har modtaget:
+
+%s
+%s
+%s
+
+        """ % (elev_txt, far_txt, besked_txt)
+                
+
+        Egreet=u"""
+Hej %s
+
+Vi glæder os til at se dig til Karmaugen 2010 :o))
+
+Du kan følge med i de seneste nyheder om stævnet på http://musikstaevner.dk .
+
+        """ % data[Enavn]
+
+
+        Fgreet=u"""
+hej %s
+
+
+%s er nu tilmeldt Danske Musikstævner 2010.
+
+For at tilmeldingen er bindende, skal du indbetale et depositum på kr. 500,- på konto nr. 3627 0002360039 inden 8 dage. Hvis du IKKE har hørt fra os 10 dage efter din tilmelding, er det fordi vi HAR modtaget din betaling..
+
+Restbeløbet skal indbetales på samme kontonummer, Senest 8 uger før stævnestart.
+
+Se danskemusikstaevner.dk. vedr. retningslinjer for tilbagebetaling af depositum og restbeløb ved afmelding.
+
+I begyndelsen af april 2010 vil du modtage detaljeret information om stævnet. Indtil da kan du holde dig ajour på http://musikstaevner.dk.
+
+
+        """ % (data[Fnavn] , data[Enavn])
+
+        Cfoot = u"Du modtager dette brev fordi vi har modtaget en tilmelding på http://musikstaevner.dk, hvis dette skulle være en fejl må du meget gerne lade os det vide på tilmelding@musikstaevner.dk"
+        
+        
+        # first send to bestyrelsen
+        bmessage = mail.EmailMessage(sender="robot@musikstaevner.dk",
+                                     subject = u"[Tilmelding] %s" % data[Enavn])
+#       bmessage.to = "bestyrelse@musikstaevner.dk"
+        bmessage.to = "mah@alslug.dk"
+        bmessage.body = u"""
+Jubii endnu en tilmelding :-D
+
+%s
+
+--
+MVH fr. robot
+
+        """ % oplysninger
+
+        bmessage.send()
+
+        # then send to elev
+        emessage = mail.EmailMessage(sender="robot@musikstaevner.dk",
+                                     to = "%s" % data[Eepost],
+                                     reply_to = "bestyrelse@musikstaevner.dk",
+                                     subject = u"tak for din tilmelding",
+                                     body = Egreet + oplysninger + Cfoot
+                                     )
+        emessage.send()
+
+        # last send to parrents/proxy pareents
+        fmessage = mail.EmailMessage(sender="robot@musikstaevner.dk",
+                                     to = "%s" % data[Fepost],
+                                     reply_to = "bestyrelse@musikstaevner.dk",
+                                     subject = u"tilmelding til Danske musikstævner 2010",
+                                     body = Fgreet + oplysninger + Cfoot
+                                     )
+        fmessage.send()
+
 
 
     def to_unicode_or_bust(self, obj, encoding='utf-8'):
@@ -47,37 +164,10 @@ class FormHandler(InboundMailHandler):
         return obj
 
 
-    def receive(self, mail_message):
-        
-        logging.basicConfig(level=logging.DEBUG)
-        self.request.charset='utf-8'
-        
+    def parse_tilmelding(self, lines):
+
         state = Header
-
-        logging.debug("mail_message= " + type(mail_message).__name__)
-        logging.debug("EncodedPayload encoding= " + str(mail_message.body.encoding))
-        logging.debug("body type= " + type(mail_message.body).__name__)
-
-        ## curcomvent bug in google api, not sure it is a safe way
-        if mail_message.body.encoding == '8bit':
-            mail_message.body.encoding = '7bit' 
-            logging.info('Body encoding fixed')
-
-        logging.info("Received a message from: "
-                     + mail_message.sender
-                     + " who wrote "
-                     + mail_message.body.decode())
-
-        
-        #we will parse the letter line for line
-        lines = mail_message.body.decode().splitlines()
-
-        logging.debug("lines type= " + type(lines).__name__)
-
-        # some version of google dev serber has an error
-        # that deliveres str instead of uniciode. Fix that
-        for line_itr in range(0, len(lines)):
-            lines[line_itr] = to_unicode_or_bust(lines[line_itr])
+        data.clear()
 
         for line in lines:
             logging.debug("line= " + type(line).__name__)
@@ -105,7 +195,7 @@ class FormHandler(InboundMailHandler):
             #we are not in the header any more,
             # so check if we need to change state
             # first see if we are in the E part
-            logging.info( "DEBUG: state = %u" %( state )   )
+            logging.debug( "state = %u" %( state )   )
             if state < 10 :
                 if line.find("Adresse:") >=  0: state = Eadr ; continue
                 if line.find("by:") >=  0: state = Epostby ; continue
@@ -117,7 +207,7 @@ class FormHandler(InboundMailHandler):
                 if line.find("relse:") >=  0: state = Eene ; continue
                 if line.find("rabat:") >=  0: state = Erabat ; continue
             else: # we are in the F part
-                logging.info("Fpart")
+                logging.debug("Fpart")
                 if line.find("navn:") >=  0: state = Fnavn ; continue
                 if line.find("Adresse:") >=  0: state = Fadr ; continue
                 if line.find("by:") >=  0: state = Fpostby ; continue
@@ -126,16 +216,12 @@ class FormHandler(InboundMailHandler):
                 if line.find("Mobil nummer:") >=  0: state = Fmnum ; continue
                 if line.find("Besked:") >=  0: state = Besked ; continue
 
-
-
-            #    print DEBUG state 
-            # besked part is different in that it has multipli lines
+            # When get here we have a data line, the state telss us where to store the data
+            # note: 'besked' part is different, in that it has multiplie lines
             if state != Besked:
                 rest = line[3:]
-                logging.info( "DEBUG:rest = %s" %(  rest)   )
-                logging.info( "DEBUG:rest = %s" %(  repr(rest))   )
+                logging.debug( "DEBUG:rest = %s" %(  rest)   )
                 data[state] = rest.rstrip()
-                logging.info( "DEBUG:%u %s" %( state , repr(data[state]))   )
             elif state == Besked:
                 if data.has_key(state):
                     data[state] += line
@@ -144,15 +230,51 @@ class FormHandler(InboundMailHandler):
             else:
                 print "problem, should newer get here"
             
-        self.store()
-                        
+
+    def receive(self, mail_message):
+        
+#        logging.basicConfig(level=logging.DEBUG)
+#        self.request.charset='utf-8'
+        
+
+        logging.debug("mail_message type= " + type(mail_message).__name__)
+        logging.debug("EncodedPayload encoding= " + str(mail_message.body.encoding))
+        logging.debug("body type= " + type(mail_message.body).__name__)
+
+        ## curcomvent bug in google api, not sure it is a safe way
+        if mail_message.body.encoding == '8bit':
+            mail_message.body.encoding = '7bit' 
+            logging.info('Body encoding fixed')
+
+        logging.info("Received a message from: "
+                     + mail_message.sender
+                     + " who wrote "
+                     + mail_message.body.decode())
+
+        
+        #we will parse the letter line for line
+        lines = mail_message.body.decode().splitlines()
+
+        logging.debug("lines type= " + type(lines).__name__)
+
+        # some version of google dev serber has an error
+        # that deliveres str instead of uniciode. Fix that
+        for line_itr in range(0, len(lines)):
+            lines[line_itr] = self.to_unicode_or_bust(lines[line_itr])
+
+        if mail_message.to.find("tilmelding") >= 0:
+            self.parse_tilmelding(lines)
+            self.store_tilmelding()
+            self.respond_tilmelding()
+        if mail_message.to.find("transport")>= 0:
+            self.parse_transport(lines)
+        if mail_message.to.find("afslutning")>= 0:
+            self.parse_afslutning(lines)
+
+        
 
 
-
-
-
-
-application = webapp.WSGIApplication([FormHandler.mapping()], debug=True)
+application = webapp.WSGIApplication([FormHandler.mapping()], debug=False)
 
 def main():
     run_wsgi_app(application)
